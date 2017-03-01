d1fd2f8a781989bb2a650e0a03bfdd49def827a9
mcdesignpro <marco.carcangiu@mcdesignpro.com>
2017-03-01 14:00:51 +0000

    array_change_key_case
    On line 41 added function to change all key in the array to lower case

--- search-engine/classes/search.class.php
+++ search-engine/classes/search.class.php
@@ -18,9 +18,11 @@ class Search {
        if($this->source && $this->source == "url"){
 
            if($this->url && $this->url !=null){
-               $dom = file_get_contents($this->url);
-               $dom = rip_tags($dom);
-               $this->content = ($dom);
+              //get content of the webpage
+              $dom = file_get_contents($this->url);
+              //strip html tags from content
+              $dom = rip_tags($dom);
+              $this->content = $dom;
            }
        }else if($this->source && $this->source == "textarea"){
 
@@ -35,16 +37,21 @@ class Search {
 
     public function countWords(){
         $this->feedback = new stdClass();
+        //count istance of a word and return array with word as key and count as value
         $words =  array_count_values(str_word_count(Search::getcontent(), 1) );
+        //change array key to lowercase
+        $words = array_change_key_case($words,CASE_LOWER);
         if($words){
 
             if(array_key_exists($this->keyword,$words)){
+
                 $wordNum = $words[$this->keyword];
                 $this->feedback->text = $this->keyword.' was found '.$wordNum.' time(s)!';
                 $this->feedback->class = 'alert alert-success';
+
             }else{
-                    $this->feedback->text = 'No words found!';
-                    $this->feedback->class = 'alert alert-danger';
+                $this->feedback->text = 'No words found!';
+                $this->feedback->class = 'alert alert-danger';
             }
 
         }else{
